24790ad3cbeb2bc1a32f7437f2334557db4e531a
Pranav Rajan <rajan.pranav320@gmail.com>
2024-05-23 18:38:44 -0700

    DAY 1 Static Arrays, Dynamic Arrays

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# interview-prep
+# README
+Preparing for technical interviews
+
+## Problem List
+Neetcode <https://neetcode.io/practice>
+
+## Arrays
+### Static Arrays
+1. Remove Duplicates From Sorted Array
+2. Remove Element
+
+### Dynamic Arrays
+3. Concatenation of Arrays
---
+++ removeElement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        k = 0 
+        
+        while i < len(nums):
+            if nums[i] == val:
+                i += 1 
+            else:
+                nums[k] = nums[i]
+                i += 1 
+                k += 1 
+        
+        return k 
+
+# runtime - O(n)
