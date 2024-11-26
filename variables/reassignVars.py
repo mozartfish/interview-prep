a86310a61a6515cd2480d349ab79f4fb720a1b02
mozartfish <rajan.pranav320@gmail.com>
2024-11-26 09:28:03 +0100

    day 1 - intro, variables

---
+++ variables/reassignVars.py
@@ -0,0 +1,10 @@
+message = "The first message"
+
+print(message)
+
+second_message = "The second message"
+print(second_message)
+
+third_message = "The third message"
+
+print(third_message)
---
+++ variables/varTypes.py
@@ -0,0 +1,13 @@
+integer_type = 25 
+float_type = 6.0
+bool_type = False 
+string_type = "hello"
+list_type = [1, 2, 3]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
