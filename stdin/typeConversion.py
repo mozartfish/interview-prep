9f2238a788f4c559b02b1c06cc84b487167f847a
mozartfish <rajan.pranav320@gmail.com>
2024-11-28 16:11:07 +0100

    Day 4 - stdin

---
+++ stdin/typeConversion.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    user_input = input()
+    return int(user_input)
+
+
+def read_float() -> float:
+    user_input = input()
+    return float(user_input)
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
