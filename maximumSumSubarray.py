e4541cbe86887b5638642fca8a887d8796170f26
mozartfish <rajan.pranav320@gmail.com>
2024-12-04 23:36:10 +0100

    module 30 - kadane's algorithm

--- maximumSumSubarray.py
+++ maximumSumSubarray.py
@@ -1,3 +1,5 @@
+# Given an array of integers nums, find the subarray with the largest sum and return the sum.
+# A subarray is a contiguous non-empty sequence of elements within an array.
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxSum = nums[0]
