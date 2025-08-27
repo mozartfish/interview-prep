68c05358064ccdf56e10070019f1491157784cba
Pranav Rajan <rajan.pranav320@gmail.com>
2025-08-27 21:49:25 +0200

    solve 3 neetcode problems

---
+++ validAnagram.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sCount = defaultdict(int)
+        tCount = defaultdict(int)
+
+        for c in s:
+            sCount[c] += 1
+
+        for c in t:
+            tCount[c] += 1
+
+        return sCount == tCount
